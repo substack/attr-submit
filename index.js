bed1497209f2019248a5cc9c98c6830a2a25c11b
James Halliday <mail@substack.net>
2014-01-15 22:12:09 -0800

    basic working module

---
+++ index.js
@@ -0,0 +1,24 @@
+var isArray = require('isarray');
+
+module.exports = function (cb) {
+    return function (elem, attr) {
+        var key = elem.getAttribute(attr);
+        elem.addEventListener('submit', function (ev) {
+            ev.preventDefault();
+            var fields = {};
+            var elems = elem.querySelectorAll('*[name]');
+            for (var i = 0; i < elems.length; i++) {
+                var value = elems[i].value;
+                var name = elems[i].getAttribute('name');
+                
+                if (fields[name] && !isArray(fields[name])) {
+                    fields[name] = [ fields[name] ]
+                }
+                
+                if (fields[name]) fields[name].push(value);
+                else fields[name] = value;
+            }
+            cb(elem, fields, key);
+        });
+    };
+};
